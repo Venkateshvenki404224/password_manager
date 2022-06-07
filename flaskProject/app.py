52ee247eea9fdc03c8c5d2eb19540cd5c135f881
Venkateshvenki404224 <75518037+Venkateshvenki404224@users.noreply.github.com>
2022-06-06 23:06:43 -0300

    Add files via upload
    Updated all the code and checked also cleared all the warnings on 6th

--- flaskProject/app.py
+++ flaskProject/app.py
@@ -1,10 +1,9 @@
-import hashlib
-from random import *
 import os
+import MySQLdb
+import bcrypt
 from flask import Flask, render_template, redirect, session, url_for, request, flash
 from flask_mysqldb import MySQL
-import MySQLdb
-from function import check,generatescreat
+from function import check, generatescreat
 
 app = Flask(__name__)
 app.secret_key = "12345678910"
@@ -12,15 +11,16 @@ app.config["MYSQL_HOST"] = "localhost"
 app.config["MYSQL_USER"] = "root"
 app.config["MYSQL_PASSWORD"] = "venki046"
 app.config["MYSQL_DB"] = "password_manager"
-
 db = MySQL(app)
 
-#adding pics
-picfolder = os.path.join('static','pics')
-app.config['UPLOAD_FOLDER']=picfolder
+# adding pics
+picfolder = os.path.join('static', 'pics')
+app.config['UPLOAD_FOLDER'] = picfolder
+
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    pics1 = os.path.join(app.config['UPLOAD_FOLDER'],'password-manager.jpg')
+    pics1 = os.path.join(app.config['UPLOAD_FOLDER'], 'password-manager.jpg')
     if request.method == 'POST':
         if 'email' in request.form and 'psw' in request.form:
             email = request.form['email']
@@ -37,23 +37,24 @@ def index():
             else:
                 flash("Enter valid email or password")
                 return redirect(url_for('index'))
-    return render_template("login.html",image = pics1)
+    return render_template("login.html", image=pics1)
 
 
 @app.route('/signup', methods=['GET', 'POST'])
 def new_user():
-    pics1 = os.path.join(app.config['UPLOAD_FOLDER'], 'SignUp.png')
+    os.path.join(app.config['UPLOAD_FOLDER'], 'SignUp.png')
     if request.method == 'POST' and "email" in request.form and "psw" in request.form and 'psw-repeat' in request.form:
         email = request.form['email']
         password = request.form['psw']
         repeat_pass = request.form['psw-repeat']
-        hashes_pass = hashlib.sha256(password.encode()).hexdigest()
-        if password == repeat_pass and check(email) == True and password!="":
+        # hashes_pass = hashlib.sha256(password.encode()).hexdigest()
+        hashed_pass = bcrypt.hashpw(password.encode('utf8'), bcrypt.gensalt())
+        if password == repeat_pass and password != "" and check(email) == True:
             cur = db.connection.cursor(MySQLdb.cursors.DictCursor)
             cur.execute("INSERT INTO `password_manager`.`users` (`emailid`, `password`) VALUES (%s,%s);",
                         (email, password))
-            cur.execute("INSERT INTO `password_manager`.`screats` (`pass_hash`, `device_secreat`) VALUES (%s,%s);",
-                        (hashes_pass, ds))
+            cur.execute("INSERT INTO `password_manager`.`screats` (`pass_hash`,`device_secreat`) VALUES (%s,%s);",
+                        (hashed_pass, ds))
             db.connection.commit()
             return redirect(url_for('index'))
     return render_template('signup.html')
@@ -61,10 +62,11 @@ def new_user():
 
 @app.route('/signup/profile')
 def profile():
-    if session['succes'] == True:
+    if session['succes']:
         return render_template("index.html")
 
-#Creating a device secreat
+
+# Creating a device secreat
 ds = generatescreat()
 
 if __name__ == '__main__':
--- flaskProject/function.py
+++ flaskProject/function.py
@@ -1,19 +1,30 @@
 import random
 import re
 import string
-from re import *
-from flask import session
 
-
+# Check Mail
 regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+
+
 def check(email):
-    # pass the regular expression
-    # and the string into the fullmatch() method
-    if re.fullmatch(regex,email):
+    if re.fullmatch(regex, email):
         return True
 
+
+# Generate a uniqe device seceret.
 def generatescreat(length=15):
-    return ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(length))
+    return ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(length))
 
-
-generatescreat()
+# def find_user():
+#     cursor = db.connection.cursor(MySQLdb.cursors.DictCursor)
+#     if request.method == 'POST':
+#         email = request.form['email']
+#         cursor.execute('SELECT * FROM users WHERE emailid=%s', (email))
+#         info = cursor.fetchone()
+#         print(info)
+#         if info is not None:
+#             if check(email) == True:
+#                 if info['emailid'] == email:
+#                     flash('Email id already in use!')
+#             else:
+#                 flash('Enter valid emaid id ')
