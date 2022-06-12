2d94f0066af117d1fd8f3d259e40729a4c1a48d6
Venkateshvenki404224 <75518037+Venkateshvenki404224@users.noreply.github.com>
2022-06-12 04:59:54 -0300

    New Files Added

---
+++ Flask-Web-App-Tutorial/website/static/index.js
@@ -0,0 +1,10 @@
+// <script>
+// function show_pass() {
+//   var x = document.getElementById("psw");
+//   if (x.type === "password") {
+//     x.type = "text";
+//   } else {
+//     x.type = "password";
+//   }
+// }
+// </script>
