c7fe96278bb5568ff45e4da7b2614146a2b36514
Venkateshvenki404224 <75518037+Venkateshvenki404224@users.noreply.github.com>
2022-06-12 23:36:28 -0300

    Add files via upload

--- Flask-Web-App-Tutorial/website/static/index.js
+++ Flask-Web-App-Tutorial/website/static/index.js
@@ -1,10 +1,31 @@
-// <script>
-// function show_pass() {
-//   var x = document.getElementById("psw");
-//   if (x.type === "password") {
-//     x.type = "text";
-//   } else {
-//     x.type = "password";
-//   }
-// }
-// </script>
+function deleteNote(noteId) {
+  fetch("/delete-note", {
+    method: "POST",
+    body: JSON.stringify({ noteId: noteId }),
+  }).then((_res) => {
+    window.location.href = "/";
+  });
+}
+var password=document.getElementById("password1","password2");
+function genPassword() {
+    var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var passwordLength = 12;
+    var password = "";
+ for (var i = 0; i <= passwordLength; i++) {
+   var randomNumber = Math.floor(Math.random() * chars.length);
+   password += chars.substring(randomNumber, randomNumber +1);
+  }
+        document.getElementById("password1").value = password;
+        document.getElementById("password2").value = password;
+ }
+var password=document.getElementById("psw");
+function Password() {
+    var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var passwordLength = 12;
+    var password = "";
+ for (var i = 0; i <= passwordLength; i++) {
+   var randomNumber = Math.floor(Math.random() * chars.length);
+   password += chars.substring(randomNumber, randomNumber +1);
+  }
+        document.getElementById("psw").value = password;
+ }
